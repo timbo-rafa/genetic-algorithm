90725ba7457b919b78198fbafaa3cca389061911
Rafael Timbo <timbo.rafa@gmail.com>
2016-09-13 17:52:01 -0300

    input examples

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python3 main.py --cities 200 \
+                --chromosomes 220 \
+                --generations 100000 \
+                --stop 4000 \
+                --exchange 200 \
+                --elite 20 \
+                --mprobability 0.20 \
+                --populations 6 \
+                --workers 4 \
+                --verbose \
