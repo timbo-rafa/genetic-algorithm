38cb04d6e6d3bb1c4f554ed17c36c924bb522595
Rafael Timbo <timbo.rafa@gmail.com>
2016-09-14 17:53:48 -0300

    argparse tests

---
+++ test/test_args.py
@@ -0,0 +1,44 @@
+from nose.tools import assert_equal, assert_not_equal
+from chromosome import Chromosome
+from args import ArgsStrings, parse_arguments
+from mock import patch
+
+from ga import GA
+from graph import Graph
+from itertools import repeat
+
+_multiprocess_can_split_ = True
+
+def test_parse_arguments_bool():
+  argstr = ArgsStrings
+  args = parse_arguments()
+  assert_equal(args.latex  , False)
+  assert_equal(args.verbose, False)
+
+def test_parse_arguments_all():
+  argstr = ArgsStrings
+  test_args = [ "./main.py"   ,
+    argstr.populations.short  , 8     ,
+    argstr.chromosomes.short  , 440   ,
+    argstr.workers.short      , 4     ,
+    argstr.generations.short  , 5555  ,
+    argstr.elite.short        , 40    ,
+    argstr.mprobability.short , 0.123 ,
+    argstr.cities.short       , 124   ,
+    argstr.exchange.short     , 66    ,
+    argstr.stop.short         , 666   ,
+    argstr.latex.short        ,
+    argstr.verbose.short      ]
+  with patch.object(sys, "argv", test_args):
+    args = parse_arguments()
+    assert_equal(args.populations , 8     )
+    assert_equal(args.chromosomes , 440   )
+    assert_equal(args.workers     , 4     )
+    assert_equal(args.generations , 5555  )
+    assert_equal(args.elite       , 40    )
+    assert_equal(args.mprobability, 0.123 )
+    assert_equal(args.cities      , 124   )
+    assert_equal(args.exchange    , 66    )
+    assert_equal(args.stop        , 666   )
+    assert_equal(args.latex       , True  )
+    assert_equal(args.verbose     , True  )
